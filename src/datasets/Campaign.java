cb903879ff121bb5f4c7ef9b41cbd9eff3be6590
ron <ron@ron-Lenovo-G40-80>
2016-07-01 23:46:20 +0200

    Bibs and bobs

--- src/datasets/Campaign.java
+++ src/datasets/Campaign.java
@@ -75,213 +75,41 @@ public class Campaign implements Cloneable
         testCampaign             = testCampaignParam;
     }
 
-    /**
-     *
-     * @return
-     */
     public int       getId()                                { return id;}
-
-    /**
-     *
-     * @return
-     */
     public Calendar  getCalendarCampaignCreated()           { return calendarCampaignCreated;}
-
-    /**
-     *
-     * @return
-     */
     public int       getOrderId()                           { return orderId;}
-
-    /**
-     *
-     * @return
-     */
     public Calendar  getCalendarScheduledStart()            { return calendarScheduledStart;}
-
-    /**
-     *
-     * @return
-     */
     public Calendar  getCalendarScheduledEnd()              { return calendarScheduledEnd;}
-
-    /**
-     *
-     * @return
-     */
     public Calendar  getCalendarExpectedStart()             { return calendarExpectedStart;}
-
-    /**
-     *
-     * @return
-     */
     public Calendar  getCalendarExpectedEnd()               { return calendarExpectedEnd;}
-
-    /**
-     *
-     * @return
-     */
     public Calendar  getCalendarRegisteredStart()           { return calendarRegisteredStart;}
-
-    /**
-     *
-     * @return
-     */
     public Calendar  getCalendarRegisteredEnd()             { return calendarRegisteredEnd;}
-
-    /**
-     *
-     * @return
-     */
     public boolean   getTestCampaign()                      { return testCampaign;}
-
-    /**
-     *
-     * @return
-     */
     public int       getTestCampaignNumber()                { if (testCampaign) {return (int)1;} else {return (int)0;}}
-
-    /**
-     *
-     * @param idParam
-     */
     public void   setId(int idParam)                                                { id                        = idParam; }
-
-    /**
-     *
-     * @param calendarCampaignCreatedParam
-     */
     public void   setCalendarCampaignCreated(Calendar calendarCampaignCreatedParam) { calendarCampaignCreated   = calendarCampaignCreatedParam; }
-
-    /**
-     *
-     * @param orderIdParam
-     */
     public void   setOrderId(int orderIdParam)                                      { orderId                   = orderIdParam; }
-
-    /**
-     *
-     * @param calendarScheduledStartParam
-     */
     public void   setCalendarScheduledStart(Calendar calendarScheduledStartParam)   { calendarScheduledStart    = calendarScheduledStartParam; }
-
-    /**
-     *
-     * @param calendarScheduledEndParam
-     */
     public void   setCalendarScheduledEnd(Calendar calendarScheduledEndParam)       { calendarScheduledEnd      = calendarScheduledEndParam; }
-
-    /**
-     *
-     * @param calendarExpectedStartParam
-     */
     public void   setCalendarExpectedStart(Calendar calendarExpectedStartParam)     { calendarExpectedStart     = calendarExpectedStartParam; }
-
-    /**
-     *
-     * @param calendarExpectedEndParam
-     */
     public void   setCalendarExpectedEnd(Calendar calendarExpectedEndParam)         { calendarExpectedEnd       = calendarExpectedEndParam; }
-
-    /**
-     *
-     * @param calendarRegisteredStartParam
-     */
     public void   setCalendarRegisteredStart(Calendar calendarRegisteredStartParam) { calendarRegisteredStart   = calendarRegisteredStartParam; }
-
-    /**
-     *
-     * @param calendarRegisteredEndParam
-     */
     public void   setCalendarRegisteredEnd(Calendar calendarRegisteredEndParam)     { calendarRegisteredEnd     = calendarRegisteredEndParam; }
-
-    /**
-     *
-     * @param testCampaignParam
-     */
     public void   setTestCampaign(boolean testCampaignParam)                        { testCampaign = testCampaignParam; }
-
-    /**
-     *
-     * @param testCampaignParam
-     */
     public void   setTestCampaignNumber(int testCampaignParam)                      { if (testCampaignParam == 0) {testCampaign = false;} else {testCampaign = true;} }
-
-    /**
-     *
-     * @param calendarCampaignCreatedEpochParam
-     */
     public void   setCalendarCampaignCreatedEpoch(Long calendarCampaignCreatedEpochParam) { calendarCampaignCreated.setTimeInMillis(calendarCampaignCreatedEpochParam);}
-
-    /**
-     *
-     * @param calendarScheduledStartEpochParam
-     */
     public void   setCalendarScheduledStartEpoch(Long calendarScheduledStartEpochParam)   { calendarScheduledStart.setTimeInMillis(calendarScheduledStartEpochParam);}
-
-    /**
-     *
-     * @param calendarScheduledEndEpochParam
-     */
     public void   setCalendarScheduledEndEpoch(Long calendarScheduledEndEpochParam)       { calendarScheduledEnd.setTimeInMillis(calendarScheduledEndEpochParam);}
-
-    /**
-     *
-     * @param calendarExpectedStartEpochParam
-     */
     public void   setCalendarExpectedStartEpoch(Long calendarExpectedStartEpochParam)     { calendarExpectedStart.setTimeInMillis(calendarExpectedStartEpochParam);}
-
-    /**
-     *
-     * @param calendarExpectedEndEpochParam
-     */
     public void   setCalendarExpectedEndEpoch(Long calendarExpectedEndEpochParam)         { calendarExpectedEnd.setTimeInMillis(calendarExpectedEndEpochParam);}
-
-    /**
-     *
-     * @param calendarRegisteredStartEpochParam
-     */
     public void   setCalendarRegisteredStartEpoch(Long calendarRegisteredStartEpochParam) { calendarRegisteredStart.setTimeInMillis(calendarRegisteredStartEpochParam);}
-
-    /**
-     *
-     * @param calendarRegisteredEndEpochParam
-     */
     public void   setCalendarRegisteredEndEpoch(Long calendarRegisteredEndEpochParam)     { calendarRegisteredEnd.setTimeInMillis(calendarRegisteredEndEpochParam);}
-
-    /**
-     *
-     */
     public void   resetCalendarCampaignCreated() { calendarCampaignCreated.setTimeInMillis(0);}
-
-    /**
-     *
-     */
     public void   resetCalendarScheduledStart()  { calendarScheduledStart.setTimeInMillis(0);}
-
-    /**
-     *
-     */
     public void   resetCalendarScheduledEnd()    { calendarScheduledEnd.setTimeInMillis(0);}
-
-    /**
-     *
-     */
     public void   resetCalendarExpectedStart()   { calendarExpectedStart.setTimeInMillis(0);}
-
-    /**
-     *
-     */
     public void   resetCalendarExpectedEnd()     { calendarExpectedEnd.setTimeInMillis(0);}
-
-    /**
-     *
-     */
     public void   resetCalendarRegisteredStart() { calendarRegisteredStart.setTimeInMillis(0);}
-
-    /**
-     *
-     */
     public void   resetCalendarRegisteredEnd()   { calendarRegisteredEnd.setTimeInMillis(0);}
 
     @Override
