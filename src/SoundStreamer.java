54062b29b3ef3e2a5b2590fae01319e0cc4b440c
ron <ronuitzaandam@gmail.com>
2018-05-11 16:34:22 +0200

    Published under: Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International Public License

--- src/SoundStreamer.java
+++ src/SoundStreamer.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright © 2008 Ron de Jong (ronuitzaandam@gmail.com).
+ *
+ * This is free software; you can redistribute it 
+ * under the terms of the Creative Commons License
+ * Creative Commons License: (CC BY-NC-ND 4.0) as published by
+ * https://creativecommons.org/licenses/by-nc-nd/4.0/ ; either
+ * version 4.0 of the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+ * Creative Commons Attribution-NonCommercial-NoDerivatives 4.0
+ * International Public License for more details.
+ *
+ * You should have received a copy of the Creative Commons 
+ * Public License License along with this software;
+ */
+
 import java.net.*;
 import java.io.IOException;
 //import java.util.*;
@@ -10,10 +29,6 @@ import javax.media.rtp.event.ReceiveStreamEvent;
 import javax.media.rtp.event.NewReceiveStreamEvent;
 import com.sun.media.rtp.*;
 
-/**
- *
- * @author ron
- */
 public class SoundStreamer implements ReceiveStreamListener
 {
     private boolean muteAudio;
